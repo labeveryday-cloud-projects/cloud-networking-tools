4f342b45d0732bb33b45bb79f0d1be3937ae9321
Du'An Lightfoot <29664932+labeveryday@users.noreply.github.com>
2024-03-11 12:13:28 -0400

    Added comments

--- get_remove_unassociated_eip.py
+++ get_remove_unassociated_eip.py
@@ -14,16 +14,32 @@ def main():
             else:
                 continue
 
-# Function that gets all regions
-def get_all_regions():
+def get_all_regions() -> list:
+    """
+    Gets all available AWS regions.
+
+    Args:
+    None:           This function takes no arguments.
+    
+    Returns:
+    list:           A list containing the name of each region as a string
+    """
     ec2 = boto3.client('ec2')
     regions = []
     for region in ec2.describe_regions()['Regions']:
         regions.append(region['RegionName'])
     return regions
 
-# Create a function that returns a list of unallocated IDs
-def get_unallocated_ids(region: str):
+def get_unallocated_ids(region: str) -> list:
+    """
+    Gets unallocated Elastic IP addresses for a region.
+
+    Args:
+    region (str):   The AWS region to check for unallocated EIPs
+
+    Returns: 
+    list:           A list containing the allocation IDs of any unassociated EIPs  
+    """
     ec2 = boto3.client('ec2', region_name=region)
     response = ec2.describe_addresses()
 
@@ -35,9 +51,19 @@ def get_unallocated_ids(region: str):
 
     return unallocated_ids
 
+def remove_unassigned_ips(region: str, allocated_id: str) -> dict:
+    """
+    Releases an unallocated Elastic IP address for a region.
 
-# Create a function that removes ip addresses that are not allocated.
-def remove_unassigned_ips(region: str, allocated_id: str):
+    Args:
+    region (str):           The AWS region containing the EIP 
+    allocated_id (str):     The allocation ID of the EIP to release
+    
+    Returns:
+    dict:                   A dictionary containing either:
+        - 'status':         'removed' and 'id': allocation_id if successful  
+        - 'status':         'failed', 'id': allocation_id, 'message': error if failed
+    """
     ec2 = boto3.client('ec2', region_name=region)
     
     try:
